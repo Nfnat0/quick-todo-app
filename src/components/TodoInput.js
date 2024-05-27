0c6fecc994efefaf23777470112f2b240c1ef363
nfnat0 <mjtmwm@gmail.com>
2024-05-28 00:09:27 +0900

    chore: Remove unused CSS class from TodoInput component

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -25,7 +25,6 @@ function TodoInput({ addTodo }) {
         variant="contained"
         color="primary"
         startIcon={<AddIcon />}
-        className="add-button"
       >
         Add
       </Button>
